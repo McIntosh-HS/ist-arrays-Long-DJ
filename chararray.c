46bbad07252902f6c4aa5550f93dd861a35c7ec8
DJ <2027ananthanarayanan.dhiraj@fbcoe.org>
2024-02-07 15:42:00 -0500

    I AM TOO GOOD

--- chararray.c
+++ chararray.c
@@ -2,13 +2,11 @@
 
 int main()
 {
-	char name[] = "First name"; // edit this line
+	char name[] = "DJ"; // edit this line
     char user[20];    // create this line and replace N with however many elements you think is necessary
-    char userhe[25];    // create this line and replace N with however many elements you think is necessary
     printf("What is your name baby ?\n");
 	scanf("%s", &user);
-    scanf("%s", &userhe);
-	printf("My name is DJ, Your name is %s %s\n", user, userhe);
+	printf("My name is %s, Your name is %s\n", name, user);
 
 	return 0;
 }
